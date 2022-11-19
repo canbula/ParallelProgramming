8a7ae01e4597aaa44ac58a03d4fa1fca2f82d058
canbula <bora.canbula@cbu.edu.tr>
2022-11-19 15:09:03 +0300

    DaemonPi before issue

---
+++ Week08/midterm/daemon_pi.py
@@ -0,0 +1,38 @@
+import threading
+import random
+
+
+class DaemonPi(threading.Thread):
+    def __init__(self, n: int = 1000):
+        super().__init__()
+        self.n = n
+        self.inner = 0
+        self.total = 0
+
+    def run(self):
+        for _ in range(self.n):
+            x = random.uniform(-1, 1)
+            y = random.uniform(-1, 1)
+            self.total += 1
+            if x ** 2 + y ** 2 <= 1:
+                self.inner += 1
+
+    def pi(self):
+        return 4 * self.inner / self.total
+
+
+def main():
+    pi_thread = DaemonPi()
+    pi_thread.start()
+    pi_thread.join()
+    while True:
+        radius = input("Enter radius: ")
+        if radius == "q":
+            break
+        print(f"Pi is estimated as {pi_thread.pi()}")
+        print(f"Area of circle with radius {radius} is "
+              f"{pi_thread.pi() * float(radius) ** 2}")
+
+
+if __name__ == '__main__':
+    main()
