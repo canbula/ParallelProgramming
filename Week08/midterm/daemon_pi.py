b4001c7372b386df1a88ad7d7961c5e17df26e0f
Fırat Köse <70978970+firatksee@users.noreply.github.com>
2022-11-19 17:22:34 +0300

    This fixes #7

--- Week08/midterm/daemon_pi.py
+++ Week08/midterm/daemon_pi.py
@@ -4,7 +4,7 @@ import random
 
 class DaemonPi(threading.Thread):
     def __init__(self, n: int = 1000):
-        super().__init__()
+        super().__init__(daemon=True)
         self.n = n
         self.inner = 0
         self.total = 0
