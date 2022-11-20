3b45cb4b1741c2d9886bd85fc5eaf3aaf097b48c
Samet <sametmertyukselmlg@gmail.com>
2022-11-20 10:28:25 +0300

    use a custom time resolution #13

--- Week08/midterm/daemon_pi.py
+++ Week08/midterm/daemon_pi.py
@@ -1,6 +1,6 @@
 import threading
 import random
-
+import time
 
 class DaemonPi(threading.Thread):
     def __init__(self, n: int = 1000):
@@ -19,6 +19,7 @@ class DaemonPi(threading.Thread):
                 self.__total += 1
                 if x ** 2 + y ** 2 <= 1:
                     self.__inner += 1
+            time.sleep(.2) # wait for a possible pi calculation request
 
     def pi(self):
         with self.lock:
@@ -35,7 +36,6 @@ def main():
         print(f"Pi is estimated as {pi_thread.pi()}")
         print(f"Area of circle with radius {radius} is "
               f"{pi_thread.pi() * float(radius) ** 2}")
-
 
 if __name__ == '__main__':
     main()
