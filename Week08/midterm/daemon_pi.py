63c47f9071cf171fd8dd31619cb1a59e9ef0e872
canbula <bora.canbula@cbu.edu.tr>
2022-11-20 19:38:54 +0300

    DaemonPi: simplified and finalized

--- Week08/midterm/daemon_pi.py
+++ Week08/midterm/daemon_pi.py
@@ -1,81 +1,52 @@
 import threading
 import random
-import math
 
 
 class DaemonPi(threading.Thread):
     def __init__(self, n: int = 1000):
-        super().__init__(daemon=True) # Fixes the open github issue #7
+        super().__init__(daemon=True)  # Fixes the open GitHub issue #7
         self.n = n
-        self._inner = 0 # Fixes the open github issue #8
-        self._total = 0 # Fixes the open github issue #8
+        # leading double underscore makes the variable private
+        self.__inner = 0  # Fixes the open GitHub issue #8
+        self.__total = 0  # Fixes the open GitHub issue #8
         self.lock = threading.Lock()
-
+        self.__loop = True  # Fixes the open GitHub issue #13
 
     def run(self):
-        while True: # Fixes the open github issue #9
+        while True:  # Fixes the open GitHub issue #9
+            if not self.__loop:  # Fixes the open GitHub issue #13
+                continue  # Fixes the open GitHub issue #13
             x = random.uniform(-1, 1)
             y = random.uniform(-1, 1)
-            with self.lock: # Fixes the open github issue #10
-                # Put the thread to sleep to allow the main thread to run
-                # time.sleep(0.0000001) # Fixes the open github issue #13
-                self._total += 1
+            with self.lock:  # Fixes the open GitHub issue #10
+                self.__total += 1
                 if x ** 2 + y ** 2 <= 1:
-                    self._inner += 1
-
+                    self.__inner += 1
 
     def pi(self):
-        with self.lock: # Fixes the open github issue #12
-            return 4 * self._inner / self._total
-
-
-    def get_total_inner(self):
-        """Returns a copy of the _total and _inner values by using the lock."""
-        with self.lock:
-            return self._total, self._inner
-
-
-    @staticmethod
-    def calculate_radius(pi, radius):
-        return pi * (float(radius) ** 2)
-
-
-    def get_current_progress(self, radius):
-        # Generate a progress bar with the _total and _inner values
-
-        total_local, inner_local = self.get_total_inner()
-        estimated_pi = self.pi()
-
-        progress_str = \
-f"""
-Gathered {total_local} points, and {inner_local} of them are inside the circle.
-* Estimated Value of PI: {estimated_pi:.30f}
-* Real Value of PI     : {math.pi:.30f}
----------------------------------------------------------
-* Estimated Radius of the circle: {self.calculate_radius(pi=estimated_pi, radius=radius):.10f}
-* Real      Radius of the circle: {self.calculate_radius(pi=math.pi, radius=radius):.10f}
-
-"""
-        print("\033c", end="") # Clears the terminal
-        print(progress_str, end="\r") # Prints the progress string
+        self.__loop = False  # Fixes the open GitHub issue #13
+        with self.lock:  # Fixes the open GitHub issue #12
+            self.__loop = True  # Fixes the open GitHub issue #13
+            print(f"Total: {self.__total}, Inner: {self.__inner}")
+            return 4 * self.__inner / self.__total
 
 
 def main():
     pi_thread = DaemonPi()
     pi_thread.start()
-    # Removed pi_thread.join() to avoid blocking the main thread, which fixes the open github issue #11
+    # Removed pi_thread.join() to avoid blocking the main thread, which fixes the open GitHub issue #11
     while True:
         radius = input("\nEnter radius: ")
         if str(radius).strip().lower() == "q":
             break
 
-        try: # Validates if the user input is a floating number.
+        try:  # Validates if the user input is a floating number.
             radius = float(radius)
         except ValueError:
             print("Please enter a valid float value for the radius. | q to quit")
             continue
 
-        pi_thread.get_current_progress(radius=radius)
+        print(f"Area of circle with radius {radius} is {pi_thread.pi() * radius ** 2}")
 
 
 if __name__ == '__main__':
