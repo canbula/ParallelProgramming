bdfd2181370494f61a3e07a7e45fa4a57c66d370
Thorakna <arbedeci@gmail.com>
2023-10-09 13:32:10 +0300

    types & sequences file generated by Onur YAŞAR

---
+++ Week02/sequences_onur.yasar.py
@@ -0,0 +1,17 @@
+my_list = [1,2,3]
+my_tuple = (1,2,3)
+my_set = {1,2,3}
+my_dict = {
+    'Test': 123,
+    'Test2': 1234,
+}
+
+
+def remove_duplicates(list):
+    pass
+
+def list_counts(list):
+    pass
+
+def reverse_dict(dict):
+    pass
