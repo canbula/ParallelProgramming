789aefa7f67abcbfda9ddb01220576bcf8e67f9b
Bora Canbula <bora.canbula@cbu.edu.tr>
2023-10-09 12:59:42 +0300

    underscore usage

---
+++ Week02/other_file.py
@@ -0,0 +1,14 @@
+def function_public():
+    print("Public Function")
+
+def _function_protected():
+    print("Protected Function")
+
+def __function_private():
+    print("Private Function")
+
+
+class OtherClass:
+    public_var = 1
+    _protected_var = 2
+    __private_var = 3
