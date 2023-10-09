0ce1c4f7e7a86a986fb0f97e2f6fd1c0a026d399
Furkan Soylek <68926724+TIMEBANDIT11111@users.noreply.github.com>
2023-10-09 23:22:18 +0300

    Create types_furkan_soylek.py

---
+++ Week02/types_furkan_soylek.py
@@ -0,0 +1,12 @@
+my_int=5
+my_float=1.2
+my_bool=True
+my_complex=8+3z
+
+"""
+###################################
+        Furkan Soylek
+https://leetcode.com/TB09/
+https://github.com/TIMEBANDIT11111
+###################################
+"""
