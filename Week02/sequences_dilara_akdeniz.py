8de85d7334f0e84e119cb8203d2726d685c5cdfe
Dilara Akdeniz <100312815+dlrakdenizz@users.noreply.github.com>
2023-10-14 18:25:00 +0300

    Update sequences_dilara_akdeniz.py

--- Week02/sequences_dilara_akdeniz.py
+++ Week02/sequences_dilara_akdeniz.py
@@ -1,6 +1,6 @@
 my_list = [1, 2, 3, 4, 5]
 my_tuple = (6, 7, 8, 9)
-my_set = {a, b, c, d}
+my_set = {"a", "b", "c", "d"}
 my_dict = {"name" : "dilara", "surname" : "akdeniz", "number" : "200315067", "gender" : "female"}
 
 def remove_duplicates(my_list):
