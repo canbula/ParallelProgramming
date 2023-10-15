78bbdd062240d4789489a6ac09a52ec085ea91a4
eagarer <erayagarer@gmail.com>
2023-10-15 22:32:00 +0300

    HW: week2 types_ & sequences

---
+++ Week02/hw/sequences_eray_agarer.py
@@ -0,0 +1,36 @@
+my_list = ["apple", "pear", "banana", "strawberry","apple"]
+my_tuple = ("linux", "windows",190316084, True)
+my_set = {"html", "css" ,"javascript",2023,2023}  
+my_dict = {
+    "name": "eray",
+    "surname":"agarer",
+    "ID":190316084
+}
+
+def remove_duplicates(list):
+    no_dublicate = []
+    for item in list:
+        if item not in no_dublicate:
+            no_dublicate.append(item)
+    return no_dublicate
+
+def list_counts(list):
+   counted_dict = {}
+
+   for item in list:
+      if item in counted_dict:
+        counted_dict[item] += 1
+      else:
+        counted_dict[item] = 1
+
+   return counted_dict
+
+def reverse_dict(dict):
+   reversed_dict = {value: key for key, value in dict.items()}
+   return reversed_dict
+
+print(remove_duplicates(my_list))
+print(list_counts(my_list))
+print(reverse_dict(my_dict))
+
+
