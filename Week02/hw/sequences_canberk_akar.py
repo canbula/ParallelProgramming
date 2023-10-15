020cec15a9e99f0a71be257641aa98809568dfa9
peritocanberkakar <canberk.akar@perito.com.tr>
2023-10-15 22:34:50 +0300

    HW:Week-2 types_&sequences_

---
+++ Week02/hw/sequences_canberk_akar.py
@@ -0,0 +1,33 @@
+my_list  = ["a","b","c","d","e"]
+my_tuple = {"canberk","akar",25}
+my_set   = {"honda","toyota","fiat","fiat"}
+my_dict={
+    "name"     :"The Sopranos",
+    "director" :"Tim Van Patten",
+    "year"     :1998
+}
+
+def remove_dublicates(list):
+    dub=[]
+    for item in list:
+        if item not in dub:
+            dub.append(item)
+    return dub
+
+def list_counts(list):
+   dictionary = {}
+   for item in list:
+      if item in dictionary:
+        dictionary[item] += 1
+      else:
+        dictionary[item] = 1
+   return dictionary
+
+def reverse_dict(dictionary):
+    reversed_dict = {}
+    for key, value in dictionary.items():
+        if value not in reversed_dict:
+            reversed_dict[value] = key
+    return reversed_dict
+
+
---
+++ Week02/hw/types_canberk_akar.py
@@ -0,0 +1,4 @@
+my_int = 2
+my_float = 5.9
+my_bool = False
+my_complex = 8 + 8j
