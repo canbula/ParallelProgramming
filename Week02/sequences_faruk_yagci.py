9d00a9a793696c30190e8926804e35d4601df3cb
farukyagci <147872095+farukyagci@users.noreply.github.com>
2023-10-15 14:47:20 +0300

    Update sequences_faruk_yagci.py

--- Week02/sequences_faruk_yagci.py
+++ Week02/sequences_faruk_yagci.py
@@ -3,13 +3,13 @@ my_tuple = (4,4,6,5,8,9,9,2,0,1)
 my_set = {9,2,6,5}
 my_dict = {'f':8,'z':9,'y':1, 'k':5}
 
-def remove_duplicates(list:list) -> list:
-  return list(set(list))
+def remove_duplicates(list_: list) -> list:
+  return list(set(list_))
 
-def list_counts(list:list) -> dict:
-  return {key: list.count(key) for key in set(list)}
+def list_counts(list_: list) -> dict:
+  return {key: list_.count(key) for key in set(list_)}
 
-def reverse_dict(d:dict) -> dict:  
+def reverse_dict(d: dict) -> dict:  
   reverseDict = {}
   for keys in d:
     values = d[keys]
