98a3a1c2f5d12a0b36b814f96f64285dbe97b29a
Mustafa Eren Tuğcu <115587894+MustafaErenTugcu@users.noreply.github.com>
2023-10-17 01:49:09 +0300

    Update v2  functions_mustafa_eren_tugcu.py

--- Week03/functions_mustafa_eren_tugcu.py
+++ Week03/functions_mustafa_eren_tugcu.py
@@ -15,7 +15,7 @@ def custom_equation(x = 0, y = 0, /, a = 1 , b = 1 , * , c  = 1 ):
     :param a : Third Number 
     :param b : Fourth Number 
     :param c : Fifth Number 
-    :return: x to the power of a, adds y to the power of b, then divides this sum by c
+    :return: result as a floating-point number.
     """
     return float((x**a + y **b ) / c)
 
@@ -24,9 +24,10 @@ def fn_w_counter():
     caller_count = defaultdict(int)
     total_calls = 0
     
-    def inner_function(caller_name):
+    def _fn_w_counter(caller_name):
         nonlocal total_calls
+        caller_name = __import__('inspect').currentframe().f_back.f_globals['__name__']
         caller_count[caller_name] += 1
         total_calls += 1
         
-    return inner_function, lambda: (total_calls, dict(caller_count))
+    return _fn_w_counter, lambda: (total_calls, dict(caller_count))
