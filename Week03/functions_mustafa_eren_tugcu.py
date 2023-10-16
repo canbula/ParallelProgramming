57915035f2aec091bd271d0641d738ba45eb46e6
Mustafa Eren Tuğcu <115587894+MustafaErenTugcu@users.noreply.github.com>
2023-10-17 02:42:40 +0300

    Update functions_mustafa_eren_tugcu.py

--- Week03/functions_mustafa_eren_tugcu.py
+++ Week03/functions_mustafa_eren_tugcu.py
@@ -19,7 +19,7 @@ def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int
     """
     return float((x**a + y **b ) / c)
 
-def fn_w_counter() -> typing.Tuple[int, typing.Dict[str, int]]:
+def fn_w_counter() -> int, dict[str, int]]:
     caller_count = defaultdict(int)
     total_calls = 0
     
