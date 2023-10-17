8510dae6d68063418ffbb2c9183632615b33b1c1
m-turan <89749358+m-turan@users.noreply.github.com>
2023-10-17 21:23:32 +0300

    WEEK3/HW decorators_murat_turan.py

--- Week03/hw/decorators_murat_turan.py
+++ Week03/hw/decorators_murat_turan.py
@@ -1,4 +1,9 @@
-def performance(fn):
+
+import tracemalloc
+from time import perf_counter
+
+
+def performance(x):
     if not hasattr(performance, "counter"):
          performance.counter = 0
     if not hasattr(performance, "total_time"):
@@ -6,21 +11,21 @@ def performance(fn):
     if not hasattr(performance, "total_mem"):
          performance.total_mem = 0
 
-    def _fn(*args, **kwargs):
+    def _performance(*args, **kwargs):
         performance.counter += 1
 
         tracemalloc.start()
         firs_time = perf_counter()
 
-        value = fn(*args, **kwargs)
+        value = x(*args, **kwargs)
 
-        last = perf_counter()
+        last_time = perf_counter()
         memory = tracemalloc.get_traced_memory()[1]
         tracemalloc.stop()
-        time = last - first_time
+        t_time = last_time - first_time
 
-        performance.total_time += time
+        performance.total_time += t_time
         performance.total_mem += memory
         return value
 
-    return _fn
+    return _performance
