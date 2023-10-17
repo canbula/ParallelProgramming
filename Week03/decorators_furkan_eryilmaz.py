9d8ed771915f71f00a3a45df58f47eb53f785779
furkane-kolta <flatline.incommunicado@gmail.com>
2023-10-17 18:54:21 +0300

    Means of memory measure changed

--- Week03/decorators_furkan_eryilmaz.py
+++ Week03/decorators_furkan_eryilmaz.py
@@ -1,18 +1,26 @@
 import time
-import sys
+import tracemalloc
 
 def performance(function_):
     def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = function_(*args, **kwargs)
-        end_time = time.time()
-
         if not hasattr(performance, 'counter'):
             performance.counter = 0
             performance.total_time = 0
             performance.total_mem = 0
 
+        start_time = time.time()
+        tracemalloc.start()
+        result = function_(*args, **kwargs)
+        traced_memory = tracemalloc.get_traced_memory()
+        if traced_memory:
+            performance.total_mem += traced_memory[1]
+
+        tracemalloc.stop()
+        end_time = time.time()
+
         performance.counter += 1
         performance.total_time += end_time - start_time
-        performance.total_mem += sys.getsizeof(result)
+
+        return result
+
     return wrapper
