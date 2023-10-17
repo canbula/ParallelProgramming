50b868144ff23473c309f1aa363fadb9b7ba0629
UtkayBattal <147428072+UtkayBattal@users.noreply.github.com>
2023-10-17 03:23:02 +0300

    Final functions_utkay_gungor_battal.py

--- Week03/functions_utkay_gungor_battal.py
+++ Week03/functions_utkay_gungor_battal.py
@@ -25,15 +25,13 @@ def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int
 
 
 def fn_w_counter() -> (int, dict[str, int]):
-    caller_count = defaultdict(int)
-    total_calls = 0
-
-    def inner_function():
-        nonlocal total_calls
-        caller_name = inspect.stack()[1].frame.f.globals['__name__']
-        caller_count[caller_name] +=1
-        total_calls +=1
-        
-
-    inner_function()
-    return inner_function
+    if not hasattr(fn_w_counter, "call_counter"):
+        fn_w_counter.call_counter = 0
+        fn_w_counter.caller_counts = {}
+    caller_name = __name__
+    fn_w_counter.call_counter += 1
+    if caller_name in fn_w_counter.caller_counts:
+        fn_w_counter.caller_counts[caller_name] += 1
+    else:
+        fn_w_counter.caller_counts[caller_name] = 1
+    return fn_w_counter.call_counter, fn_w_counter.caller_counts
