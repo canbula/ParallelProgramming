d1f183a1dd0873df055609dbc793f886ee70d826
UtkayBattal <147428072+UtkayBattal@users.noreply.github.com>
2023-10-19 23:09:16 +0300

    Create decorators_utkay_gungor_battal.py

---
+++ Week03/decorators_utkay_gungor_battal.py
@@ -0,0 +1,32 @@
+import time 
+import tracemalloc
+
+def performance(fn):
+    if not hasattr(performance, "counter"):
+         performance.counter = 0
+    if not hasattr(performance, "total_time"):
+         performance.total_time = 0
+    if not hasattr(performance, "total_mem"):
+         performance.total_mem = 0
+   
+  
+    def _fn (*args, **kwargs):
+      performance.counter += 1
+
+      tracemalloc.start()
+      start_time = perf_counter()
+
+      value = per(*args, **kwargs)
+
+      finish_time = perf_counter()
+      max_mem = tracemalloc.get_traced_memory()[1]
+      tracemalloc.stop()
+      d_time = finish_time - start_time
+
+      perfomance.total_time = d_time
+      performance.total_mem = max_mem
+      return value
+
+return _fn
+      
+      
