3a143b92a2b6c4f9d571f89b619347635cb7d55f
UtkayBattal <147428072+UtkayBattal@users.noreply.github.com>
2023-10-19 23:14:04 +0300

    Update decorators_utkay_gungor_battal.py

--- Week03/decorators_utkay_gungor_battal.py
+++ Week03/decorators_utkay_gungor_battal.py
@@ -1,5 +1,6 @@
 import time 
 import tracemalloc
+from time import perf_counter
 
 def performance(fn):
     if not hasattr(performance, "counter"):
@@ -16,15 +17,15 @@ def performance(fn):
       tracemalloc.start()
       start_time = perf_counter()
 
-      value = per(*args, **kwargs)
+      value = fn(*args, **kwargs)
 
       finish_time = perf_counter()
       max_mem = tracemalloc.get_traced_memory()[1]
       tracemalloc.stop()
       d_time = finish_time - start_time
 
-      perfomance.total_time = d_time
-      performance.total_mem = max_mem
+      perfomance.total_time += d_time
+      performance.total_mem += max_mem
       return value
 
 return _fn
