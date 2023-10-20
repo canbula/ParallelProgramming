a92e9fb9e6adb0f78f1a8331a81d68a8ec13e62c
Burak Güllüler <85554705+buraxta@users.noreply.github.com>
2023-10-20 18:56:39 +0300

    the attributes changed

--- Week03/decorators_burak_gulluler.py
+++ Week03/decorators_burak_gulluler.py
@@ -9,15 +9,15 @@ def performance(fn):
         end_time = time.perf_counter()  # Finish measuring the time of the process
         mem_used = tracemalloc.get_traced_memory()[1]  # Get the traced memory usage
 
-        if not hasattr(wrapped_function, 'counter'):
+        if not hasattr(performance, 'counter'):
             # If 'counter' attribute doesn't exist in the wrapped function, initialize it along with 'total_time' and 'total_mem'
-            wrapped_function.counter = 0
-            wrapped_function.total_time = 0
-            wrapped_function.total_mem = 0
+            performance.counter = 0
+            performance.total_time = 0
+            performance.total_mem = 0
 
-        wrapped_function.counter += 1  # Increment the counter for each function call
-        wrapped_function.total_time += end_time - start_time  # Update the total time with the current function's execution time
-        wrapped_function.total_mem += mem_used  # Update the total memory usage with the current function's memory usage
+        performance.counter += 1  # Increment the counter for each function call
+        performance.total_time += end_time - start_time  # Update the total time with the current function's execution time
+        performance.total_mem += mem_used  # Update the total memory usage with the current function's memory usage
         return result  # Return the result of the wrapped function
 
     return wrapped_function  # Return the wrapped function as the decorator
