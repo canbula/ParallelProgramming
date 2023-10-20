df06354f4f782ef569fac251a1154cbf41e3e526
berkeyildiz0 <148001533+berkeyildiz0@users.noreply.github.com>
2023-10-20 14:35:41 +0300

    Update decorators_berke_yildiz.py

--- Week03/decorators_berke_yildiz.py
+++ Week03/decorators_berke_yildiz.py
@@ -1,18 +1,18 @@
 import tracemalloc,time
 
-def decorator(fn):
-    if not hasattr(decorator,"counter"):
-            decorator.counter = 0
-            decorator.total_time = 0
-            decorator.total_mem = 0
-    def _decorator(*args,**kwargs):
-            decorator.counter += 1
+def performance(fn):
+    if not hasattr(performance,"counter"):
+            performance.counter = 0
+            performance.total_time = 0
+            performance.total_mem = 0
+    def _performance(*args,**kwargs):
+            performance.counter += 1
             start_time = time.time()
             tracemalloc.start()
             fn(*args,**kwargs)
             end_time = time.time()
-            decorator.total_mem += tracemalloc.get_traced_memory()
+            performance.total_mem += tracemalloc.get_traced_memory()
             tracemalloc.stop()
             exec_time = round(end_time - start_time,ndigits=4)
-            decorator.total_time += exec_time
-    return _decorator
+            performance.total_time += exec_time
+    return _performance
