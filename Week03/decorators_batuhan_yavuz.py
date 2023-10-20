08e1fac6940c2c122a7f22ed5f903c08b71b1cef
Batuhan Yavuz <92747705+batuhan023@users.noreply.github.com>
2023-10-20 19:38:34 +0300

    Update decorators_batuhan_yavuz.py

--- Week03/decorators_batuhan_yavuz.py
+++ Week03/decorators_batuhan_yavuz.py
@@ -1,14 +1,17 @@
 import time,tracemalloc
 def performance(func):
-    def wrapper():
-        if not hasattr(wrapper,'counter'):
-            wrapper.counter = 1
-            wrapper.total_time = 0
-            wrapper.total_mem = 0
+    def wrapper(*args,**kwargs):
+        if not hasattr(performance,'counter'):
+            performance.counter = 0
+            performance.total_time = 0
+            performance.total_mem = 0
+
+        performance.counter +=1
         start = time.time()
         tracemalloc.start()
         func()
-        wrapper.total_mem = tracemalloc.get_traced_memory()[1]
-        wrapper.total_time = time.time()-start
-   
+        performance.total_mem = tracemalloc.get_traced_memory()[1]
+        performance.total_time = time.time()-start
+
+      
     return  wrapper
