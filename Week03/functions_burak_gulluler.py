06da3f11bcf62ebc95751a7a5f80e615c17d3acc
Burak Güllüler <85554705+buraxta@users.noreply.github.com>
2023-10-20 19:05:31 +0300

    function attributes added

--- Week03/functions_burak_gulluler.py
+++ Week03/functions_burak_gulluler.py
@@ -4,7 +4,6 @@ custom_power = lambda x=0, /, e=1: x**e
 def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int = 1) -> float:
     """
     a function that takes 5 arguments, 2 of which are optional, and returns a float
-
     :param x: (int) first value
     :param y: (int) second value
     :param a: (int) first exponent
@@ -15,18 +14,25 @@ def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int
     return (x**a + y**b) / c
 
 
-call_counter = 0
-call_counts = {}
+
 
 def fn_w_counter() -> (int, dict[str, int]):
-    global call_counter
-    call_counter += 1
-
+    # Get the caller's name
     caller = fn_w_counter.__name__
 
-    if caller in call_counts:
-        call_counts[caller] += 1
-    else:
-        call_counts[caller] = 1
+    # Initialize counters if they don't exist
+    if not hasattr(fn_w_counter, 'call_counter'):
+        fn_w_counter.call_counter = 0
+        fn_w_counter.caller_counts = {}
+    
+    # Increment the call counter
+    fn_w_counter.call_counter += 1
 
-    return call_counter, call_counts
+    # Increment the caller-specific counter
+    if caller in fn_w_counter.caller_counts:
+        fn_w_counter.caller_counts[caller] += 1
+    else:
+        fn_w_counter.caller_counts[caller] = 1
+
+    # Return the results
+    return fn_w_counter.call_counter, fn_w_counter.caller_counts
