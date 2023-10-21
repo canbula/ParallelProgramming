d8038ff82d5f3db469a05345ee8d2bd185c56dda
atakankarakoc <96680387+atakankarakoc@users.noreply.github.com>
2023-10-21 13:08:55 +0300

    Update decorators_atakan_karakoc.py

--- Week03/decorators_atakan_karakoc.py
+++ Week03/decorators_atakan_karakoc.py
@@ -16,31 +16,18 @@ def performance(func):
         :param kwargs: Function keyword arguments
         """
         start_time = time.perf_counter()
-        # print(f"start time: {start_time}")
         tracemalloc.start()
+        
         performance.counter += 1
-        # print(f"counter: {performance.counter}")
+        
         func(*args, **kwargs)
+        
         end_time = time.perf_counter()
-        # print(f"end_time: {end_time}")
         tracemalloc.stop()
         mem_usage = tracemalloc.get_traced_memory()
+        
         performance.total_mem += mem_usage[1]
-        # print(f"total memory: {performance.total_mem}")
         performance.total_time += (end_time - start_time)
-        # print(f"total time: {performance.total_time}")
-
+        
     return _performance
 
-
-@performance
-def performance_Test():
-    """
-    Example function performance_Test for performance measurement.
-    This function is an example that demonstrates how to use
-    the performance decorator.
-    """
-    print("----")
-
-
-performance_Test()
