39fb48aadcca461334e139c62765cf81f7e61c0b
Mehmet Can Tekin <83635338+Atreox@users.noreply.github.com>
2023-10-22 16:44:06 +0300

    Update functions_mehmet_can_tekin.py

--- Week03/functions_mehmet_can_tekin.py
+++ Week03/functions_mehmet_can_tekin.py
@@ -1,6 +1,6 @@
 custom_power = lambda x = 0 ,/, e=1 : x**e
 
-def custom_equation(x = 0, y = 0 , / , a = 1 , b = 1 , * ,c =1 )->float:
+def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int = 1) -> float:
     """
     The function compute the result of the :((x**a + y**b) /c).
     :param x: First positional-only integer (default=0)
