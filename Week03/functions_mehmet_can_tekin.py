c8bb07b68bb515b34a3bacb0845582d69fa3eb8b
Mehmet Can Tekin <83635338+Atreox@users.noreply.github.com>
2023-10-22 15:57:48 +0300

    Create functions_mehmet_can_tekin.py

---
+++ Week03/functions_mehmet_can_tekin.py
@@ -0,0 +1,26 @@
+custom_power = lambda x = 0 ,/, e=1 : x**e
+
+def custom_equation(x = 0, y = 0 , / , a = 1 , b = 1 , * ,c =1 ):
+    """
+    The function compute the result of the :((x**a + y**b) /c).
+    :param x: First positional-only integer (default=0)
+    :param y: Second positional-only integer (default=0)
+    :param a: First positional-or-keyword integer (default=1)
+    :param b: Second positional-or-keyword integer (default=1)
+    :param c: First keyword-only integer(default=1)
+    :return: ((x**a + y**b)/c)
+    """
+    return (x**a + y**b )/c
+
+
+def fn_w_counter() -> (int, {str, int}):
+    if not hasattr(fn_w_counter, "count"):
+        fn_w_counter.count = 0
+        fn_w_counter._dict = {}
+    caller_name = __name__
+    fn_w_counter.count += 1
+    if caller_name in fn_w_counter._dict:
+        fn_w_counter._dict[caller_name] += 1
+    else:
+        fn_w_counter._dict[caller_name] = 1
+    return fn_w_counter.count, fn_w_counter._dict
