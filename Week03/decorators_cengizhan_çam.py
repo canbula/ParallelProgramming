2cb1d762af916040aa36463f607d42068f3bf000
cengizhancam <camcengizhan@gmail.com>
2023-10-22 19:36:28 +0300

    Create decorators_cengizhan_çam.py

---
+++ Week03/decorators_cengizhan_çam.py
@@ -0,0 +1,24 @@
+import time
+import tracemalloc
+
+def performance(func):
+    if not hasattr(performance, "counter"):
+        performance.counter = 0
+        performance.total_time = 0
+        performance.total_mem = 0
+
+    def wrapped_function(*args, **kwargs):
+        start_time = time.perf_counter()
+        performance.counter += 1
+        tracemalloc.start()
+
+        func(*args, **kwargs)
+
+        end_time = time.perf_counter()
+        performance.total_mem += tracemalloc.get_traced_memory()[1]
+        tracemalloc.stop()
+        performance.total_time += (end_time-start_time)
+
+
+    return wrapped_function()
+  
