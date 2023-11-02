d6819a2b3497e81d417ddd78a6f7896d73cf782c
BarisZirhli <61722105+BarisZirhli@users.noreply.github.com>
2023-11-02 16:52:03 +0300

    Update types_baris_zirhli.py

--- Week02/hw/types_baris_zirhli.py
+++ Week02/hw/types_baris_zirhli.py
@@ -2,10 +2,3 @@ my_int = 314
 my_float = 3.14
 my_bool = True
 my_complex = 3+14j
-
-my_list = [1, 2, 3, 4, 5]
-my_tuple = (1, 2, 3, 4, 5)
-my_set = {1, 2, 3, 4, 5}
-my_dict = {1: "California Love", 2: "West Coast",
-           3: "Long Beach", 4: "Dr. Dre", 5: "Hollywood Dream"}
-
