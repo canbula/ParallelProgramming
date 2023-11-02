ea05027d814980bc027075e91b391ccb1b6e66d5
Kübra Çelebi <72304467+kubraca@users.noreply.github.com>
2023-11-02 21:40:45 +0300

    Update sequences_kubra_celebi.py
    Fixed indentation errors

--- Week02/sequences_kubra_celebi.py
+++ Week02/sequences_kubra_celebi.py
@@ -1,25 +1,24 @@
+
 my_list=[9,10,True,"k"]
 my_tuple=(3,5,7)
 my_set={2,4,1}
-my_dict={"name" :"Kubra",
-        "surname" : "Celebi",
+my_dict={"name":"Kubra",
+        "surname":"Celebi",
         "city":"Kocaeli"}
-
 def remove_duplicates(first_list):
-empty_list=[]
-for item in first_list:
-  if item not in empty_list:
-    empty_list.appent(item)
-
-return empty_list
+    empty_list=[]
+    for item in first_list:
+        if item not in empty_list:
+            empty_list.append(item)
+    return empty_list
 
 def list_counts(first_list):
-  dict_counts={}
-  for item in first_list:
-      if item in dict_counts:
-          dict_counts[item] +=1
-      else:
-          dict_counts[item]=1
+    dict_counts={}
+    for item in first_list:
+        if item in dict_counts:
+            dict_counts[item] +=1
+        else:
+            dict_counts[item]=1
     return dict_counts
 
 def reverse_dict(first_dict):
@@ -27,5 +26,13 @@ def reverse_dict(first_dict):
   for key, value in first_dict.items():
     containing_inversions[value] = key
   return containing_inversions
-        
+
+def main_function():
+    print(remove_duplicates(my_list))
+    print(list_counts(my_list))
+    print(reverse_dict(my_dict))
+
+main_function()
+
+
       
