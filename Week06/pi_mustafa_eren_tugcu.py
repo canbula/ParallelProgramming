a004bf72dea911ec11cdc2ea31cc29c02b6644ef
Mustafa Eren Tuğcu <115587894+MustafaErenTugcu@users.noreply.github.com>
2023-11-06 12:03:17 +0300

    Update pi_mustafa_eren_tugcu.py

--- Week06/pi_mustafa_eren_tugcu.py
+++ Week06/pi_mustafa_eren_tugcu.py
@@ -1,19 +1,22 @@
 import random 
 
-
 def next_pi(): 
-    n = 100
-    i  = 0 
-
-    for _ in range(n): 
+    n = 1000
+    i = 0 
+    
+    while True: 
         x = random.uniform(-1,1)
         y = random.uniform(-1,1)    
-    
         r = x**2 + y**2 
 
         if(r <= 1 ) : 
             i += 1 
         
         pi = 4 * i / n 
-        
         yield pi 
+        
+generator = next_pi()
+iterator = iter(generator) 
+
+while True: 
+    print(iterator.__next__())
