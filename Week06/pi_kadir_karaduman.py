a584d37b2ad5076ed5d6c8918b7f08d8ff6bd32d
Kadir Karaduman <40742246+kadirkaraduman@users.noreply.github.com>
2023-11-06 12:05:50 +0300

    Update pi_kadir_karaduman.py

--- Week06/pi_kadir_karaduman.py
+++ Week06/pi_kadir_karaduman.py
@@ -2,12 +2,12 @@ import random
 
 def next_pi():
 
-    n = 1000
+    
     
     points_in_circle = 0
     points_in_total = 0
-
-    for _ in range(n):
+    
+    while True:
         x = random.uniform(0,1)
         y = random.uniform(0,1)
 
@@ -19,4 +19,4 @@ def next_pi():
 
         yield 4 * points_in_circle / points_in_total
 
-    raise StopIteration
+    
