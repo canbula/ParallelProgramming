a425d81b893c9250b529dfecdd849d91e1748cc4
Mustafa Eren Tuğcu <115587894+MustafaErenTugcu@users.noreply.github.com>
2023-11-06 11:34:34 +0300

    pi_mustafa_eren_tugcu.py

--- Week06/pi_mustafa_eren_tugcu.py
+++ Week06/pi_mustafa_eren_tugcu.py
@@ -1,19 +1,17 @@
 import random 
 
-n = int(input("Enter the number of points: "))
-i  = 0 
 
-for _ in range(n): 
-    x = random.uniform(-1,1)
-    y = random.uniform(-1,1)    
+def next_pi(): 
+    n = int(input("Enter the number of points: "))
+    i  = 0 
+
+    for _ in range(n): 
+        x = random.uniform(-1,1)
+        y = random.uniform(-1,1)    
     
-    r = x**2 + y**2 
+        r = x**2 + y**2 
 
-    if(r <= 1 ) : 
-        print("inside the circle")
-        i += 1 
-    else : 
-        print("outside the circle") 
-
-pi = 4 * i / n 
-print("pi = ", pi  , "i=" , i , "n=" , n)    
+        if(r <= 1 ) : 
+            i += 1 
+        
+        pi = 4 * i / n 
