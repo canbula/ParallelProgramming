ee48bccbf1b1c72cef366ec168736f1b067e30b9
Beyza Sungar <107926225+bsungar@users.noreply.github.com>
2023-11-06 12:05:27 +0300

    Update pi_beyza_sungar.py

--- Week06/pi_beyza_sungar.py
+++ Week06/pi_beyza_sungar.py
@@ -1,26 +1,19 @@
 import random
 
 def next_pi():
-    circle_points = 0
-    square_points = 0
-    interval = 50
+    inner_points = 0
+    total_points = 0
+    
 
-    for _ in range(interval):
+    while True:
         x = random.uniform(-1, 1)
         y = random.uniform(-1, 1)
         if (x**2 + y**2) < 1:
-            circle_points += 1
-        square_points += 1
+            inner_points += 1
+        total_points += 1
 
-        pi = 4 * (circle_points / square_points)
+        pi = 4 * (inner_points / total_points)
         yield pi
 
 generator = next_pi()
 iterator = iter(generator)
-
-try:
-    while True:
-        next_value = next(iterator)
-        print(next_value)
-except StopIteration:
-    raise StopIteration
