ea4424567f84aa704617bfb785d80a73175e25df
einsteinpikachu <93139894+einsteinpikachu@users.noreply.github.com>
2023-11-06 11:56:45 +0300

    Add files via upload

---
+++ Week06/pi_mehmet_osman_akgun.py
@@ -0,0 +1,24 @@
+import random
+
+def Generator():
+    inner = 0
+    total = 0
+
+    while True:
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        total += 1
+
+        distance = x ** 2 + y ** 2
+
+        if distance <= 1:
+            inner += 1
+
+        estimated_pi = (inner / total) * 4
+        yield estimated_pi, total
+
+def next_pi(pi_gen):
+    return next(pi_gen)
+
+pi_generator = Generator()
+
