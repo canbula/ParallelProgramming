731f18e32e29125a7bc631624ca2e968ba07aa49
Beyza Sungar <107926225+bsungar@users.noreply.github.com>
2023-11-06 12:17:21 +0300

    Update pi_beyza_sungar.py

--- Week06/pi_beyza_sungar.py
+++ Week06/pi_beyza_sungar.py
@@ -3,11 +3,10 @@ import random
 def next_pi():
     inner_points = 0
     total_points = 0
-    
 
     while True:
-        x = random.uniform(-1, 1)
-        y = random.uniform(-1, 1)
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
         if (x**2 + y**2) < 1:
             inner_points += 1
         total_points += 1
