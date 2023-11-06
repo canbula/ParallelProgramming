7fd89876db7cc4c31790de4fc264fef9f444986c
Mustafa Eren Tuğcu <115587894+MustafaErenTugcu@users.noreply.github.com>
2023-11-06 12:14:47 +0300

    Update pi_mustafa_eren_tugcu.py

--- Week06/pi_mustafa_eren_tugcu.py
+++ Week06/pi_mustafa_eren_tugcu.py
@@ -2,19 +2,18 @@ import random
 
 def next_pi(): 
    
-  
     i = 0
     n = 0 
     
     while True: 
-        x = random.uniform(-1,1)
-        y = random.uniform(-1,1)    
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)    
         r = x**2 + y**2 
 
-        if(r <= 1 ) : 
+        if r <= 1 : 
             i += 1 
         n += 1  
         
-        pi = 4 * i / n 
-        yield pi 
+        
+        yield 4 * i / n
         
