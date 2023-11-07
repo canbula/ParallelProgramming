911a1fc6676ab990265a16decd63373cf586d746
RukiyeTura <124088168+RukiyeTura@users.noreply.github.com>
2023-11-07 17:04:40 +0300

    Update pi_rukiye_tura.py

--- Week06/pi_rukiye_tura.py
+++ Week06/pi_rukiye_tura.py
@@ -1,11 +1,11 @@
 import random
 
-def next_pi(total):
+def next_pi():
     points_in_circle = 0
-
-    for i in range(total):
+    points_out_circle = 0 
+    while True: 
         x, y = random.random(), random.random()
         if x**2 + y**2 <= 1:
             points_in_circle += 1
-
-        yield 4 * points_in_circle / (i + 1)
+        points_out_circle += 1 
+        yield 4 * points_in_circle / points_out_circle 
