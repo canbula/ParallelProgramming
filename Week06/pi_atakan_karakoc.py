10f18acf7b236f997d00b089c00c5dc484de49f5
atakankarakoc <96680387+atakankarakoc@users.noreply.github.com>
2023-11-08 12:56:28 +0300

    Update pi_atakan_karakoc.py
    class removed

--- Week06/pi_atakan_karakoc.py
+++ Week06/pi_atakan_karakoc.py
@@ -1,33 +1,30 @@
 import random
 
-class Pi:
-    def __init__(self):
-        self.outer = 0
-        self.inner = 0
+# class Pi:
+#     def __init__(self):
+#         self.outer = 0
+#         self.inner = 0
 
-    def get_outer(self):
-        return self.outer
+#     def get_outer(self):
+#         return self.outer
 
-    def get_inner(self):
-        return self.inner
+#     def get_inner(self):
+#         return self.inner
 
-    def next_pi(self):
-        while True:
-            rand_x, rand_y = random.random(), random.random()
-            if rand_x ** 2 + rand_y ** 2 <= 1:
-                self.inner += 1
-            self.outer += 1
-            pi = 4 * (self.inner / self.outer)
-            yield pi
+def next_pi():
+    inner, outer = 0, 0
+    while True:
+        rand_x, rand_y = random.random(), random.random()
+        if rand_x ** 2 + rand_y ** 2 <= 1:
+            inner += 1
+        outer += 1
+        pi = 4 * (inner / outer)
+        yield pi
 
 
 if __name__ == '__main__':
-    p = Pi()
-    pi_generator = p.next_pi()
+    pi_generator = next_pi()
 
     for i in range(10000):
-        print(next(pi_generator))
-        # print(f"Outer:{p.get_outer()} - Inner: {p.get_inner()} - pi: {next(pi_generator)}")
-
-    print(f"Total Outer: {p.get_outer()} ----- Total Inner: {p.get_inner()}")
+        print(f"{next(pi_generator)}")
 
