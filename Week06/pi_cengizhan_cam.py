88232d6bd209474ecd83d094037b3ae95ca3f13e
cengizhancam <camcengizhan@gmail.com>
2023-11-08 15:21:40 +0300

    Create pi_cengizhan_cam.py

---
+++ Week06/pi_cengizhan_cam.py
@@ -0,0 +1,16 @@
+import random
+
+def next_pi() :
+    num_in=0
+    total_num=0
+
+    while true :
+        x=random.uniform(-1,1)
+        y=random.uniform(-1,1)
+
+        if x**2 + y**2 <= 1 :
+            num_in +=1
+        
+        total_num +=1
+
+        yield 4* num_in/total_num
