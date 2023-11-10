34ce587d60f19e07032623c8232e37ec3ea14ca2
BarisZirhli <61722105+BarisZirhli@users.noreply.github.com>
2023-11-11 01:36:47 +0300

    pi_baris_zirhli.py

---
+++ Week06/pi_baris_zirhli.py
@@ -0,0 +1,19 @@
+import random
+
+def next_pi() -> None:
+    inner, outter = 0, 0
+    while True:
+        randomX, randomY = random.random(), random.random()
+        if randomX**2 + randomY**2 <= 1:
+            inner += 1
+        outter += 1
+
+        phi = 4 * (inner / outter)
+        yield pi
+
+
+if __name__ == "__main__":
+    pi = next_pi()
+
+    for i in range(10000000):
+        print(f"{next(pi)}")
