e09d57567c96ccead6f3451dc78640dbe41a7df8
icelal-kskn <72507616+icelal-kskn@users.noreply.github.com>
2024-09-26 11:23:48 +0300

    Update types_Ikram_Celal_Keskin.py
    renamed types_Ikram_Celal_Keskin to types_ikram_celal_keskin and removed overused comment

--- Week02/types_Ikram_Celal_Keskin.py
+++ Week02/types_Ikram_Celal_Keskin.py
@@ -1,10 +1,4 @@
-
-
-# 1. Create a variable named my_int and assign the value 27 to it.
 my_int=27
-# 2. Create a variable named my_float and assign the value 0.6 to it.
 my_float=0.6
-# 3. Create a variable named my_bool and assign the value True to it.
 my_bool=True
-# 4. Create a variable named my_complex and assign the value "2000+2j" to it.
 my_complex=2000+2j
