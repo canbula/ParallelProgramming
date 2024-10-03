487c60deec41a174f4852f276dda4fb1f65dc1b6
zeki akgül <121755454+zexy2@users.noreply.github.com>
2024-10-03 21:36:04 +0300

    Create pyramid_zeki_akgul.py

---
+++ Week03/pyramid_zeki_akgul.py
@@ -0,0 +1,39 @@
+
+def calculate_pyramid_height(number_of_blocks):
+    """
+    Calculates the maximum height of a pyramid that can be built with a given number of blocks.
+    
+    The pyramid is built layer by layer, where each layer requires an increasing number of blocks.
+    The first layer requires 1 block, the second layer 2 blocks, and so on. The function subtracts 
+    the blocks needed for each layer until there are not enough blocks for the next layer.
+    
+    Parameters:
+    number_of_blocks (int): The total number of blocks available to build the pyramid.
+
+    Returns:
+    int: The maximum height of the pyramid that can be built.
+    If the input is invalid (not an integer), an error message is printed.
+    """
+  
+    # Initial height and number of blocks required for the first layer
+    height = 0
+    number = 1
+    
+    # Check if the input is an integer
+    if isinstance(number_of_blocks, int):
+        # If the number of blocks is less than 3, a pyramid cannot be built
+        if number_of_blocks < 3:
+            return height
+        else:
+            # Continue building the pyramid as long as there are enough blocks for the next layer
+            while number_of_blocks >= number:
+                number_of_blocks -= number
+                number += 1
+                height += 1
+            return height
+    else:
+        print("Invalid parameter! Parameter must be an integer")
+        return None
+      
+# Test the function       
+calculate_pyramid_height(15)
