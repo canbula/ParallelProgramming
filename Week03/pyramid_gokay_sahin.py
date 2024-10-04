0666e3b82e10f8050c671dc47c696c55226fb08d
Katex <67821908+katex35@users.noreply.github.com>
2024-10-04 16:20:19 +0300

    Create pyramid_gokay_sahin.py

---
+++ Week03/pyramid_gokay_sahin.py
@@ -0,0 +1,13 @@
+def calculate_pyramid_height(number_of_blocks):
+    height = 0
+
+    if number_of_blocks == 1:
+        return number_of_blocks
+
+    for i in range(1, number_of_blocks):
+        print(i)
+        if number_of_blocks >= i:
+            number_of_blocks = number_of_blocks - i
+            height += 1
+
+    return height
