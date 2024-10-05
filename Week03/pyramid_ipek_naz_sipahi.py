87b1345246acc925880ed0462c6a12b837c6b026
İpek Naz Sipahi <142670058+4idoneus@users.noreply.github.com>
2024-10-05 10:21:17 +0300

    Create pyramid_ipek_naz_sipahi.py

---
+++ Week03/pyramid_ipek_naz_sipahi.py
@@ -0,0 +1,8 @@
+def calculate_pyramid_height(number_of_blocks):
+    height = 0 
+    total_blocks = 0  # Total number of blocks used so far
+    while total_blocks + (height + 1) <= number_of_blocks: 
+        height += 1  
+        total_blocks += height  # Add the number of blocks in the new level to the total
+    print(f"The pyramid can have {height} levels.") 
+    return height 
