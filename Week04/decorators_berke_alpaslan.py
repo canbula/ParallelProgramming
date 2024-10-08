eda3a3d6d32c14c50062a6bc0c9224aae2eb204c
Berke Alpaslan <93981165+BerkeAlpaslan@users.noreply.github.com>
2024-10-08 09:42:12 +0300

    Create decorators_berke_alpaslan.py

---
+++ Week04/decorators_berke_alpaslan.py
@@ -0,0 +1,22 @@
+import time
+import tracemalloc
+
+
+def performance(func):
+    setattr(performance, 'counter', 0)
+    setattr(performance, 'total_time', 0.0)
+    setattr(performance, 'total_mem', 0.0)
+
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        setattr(performance, 'counter', getattr(performance, 'counter') + 1)
+        setattr(performance, 'total_time', getattr(performance, 'total_time') + end_time - start_time)
+        setattr(performance, 'total_mem', getattr(performance, 'total_mem') + peak)
+        return result
+
+    return wrapper
