0e56ebf32a9b163abcf2287a3bde29cf22411b2c
Özgül BAYTEKİN <125576234+ozgulbaytekin@users.noreply.github.com>
2024-10-08 19:57:23 +0300

    Update pyramid_ozgul_baytekin.py

--- Week03/pyramid_ozgul_baytekin.py
+++ Week03/pyramid_ozgul_baytekin.py
@@ -1,8 +1,9 @@
 def calculate_pyramid_height(number_of_blocks):
-    height = 1  # there will be 1 block for the first level of the pyramid
-    while number_of_blocks >= height:
-        number_of_blocks -= height
-        height += 1
-    return height - 1
+    height = 1  # there will be 1 block for the first level
+    while number_of_blocks >= height:  
+        number_of_blocks -= height  
+        height += 1 
+        
+    return height -1
 
-# print(calculate_pyramid_height(int(input("Please enter the number of blocks: "))))
+
