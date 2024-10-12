52c13b266dbfe69ac735cacd1f50a8e4cf120df2
Extocraftor <51536864+Extocraftor@users.noreply.github.com>
2024-10-12 14:36:40 +0300

    Create decorators_mert_durgun.py

---
+++ Week04/decorators_mert_durgun.py
@@ -0,0 +1,33 @@
+import time
+import tracemalloc
+
+def performance_decorator(func):
+    call_count = 0
+    total_time = 0.0
+    total_memory = 0.0
+
+    def wrapped_function(*args, **kwargs):
+        nonlocal call_count, total_time, total_memory
+        
+        tracemalloc.start()
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        current_memory, peak_memory = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        
+        call_count += 1
+        total_time += end_time - start_time
+        total_memory += peak_memory
+
+        wrapped_function.call_count = call_count
+        wrapped_function.total_time = total_time
+        wrapped_function.total_memory = total_memory
+
+        return result
+
+    wrapped_function.call_count = call_count
+    wrapped_function.total_time = total_time
+    wrapped_function.total_memory = total_memory
+
+    return wrapped_function
