556f959bb72b11e9c401921fd5bc731c15d75354
Extocraftor <51536864+Extocraftor@users.noreply.github.com>
2024-10-12 14:58:00 +0300

    Update decorators_mert_durgun.py

--- Week04/decorators_mert_durgun.py
+++ Week04/decorators_mert_durgun.py
@@ -2,32 +2,24 @@ import time
 import tracemalloc
 
 def performance(func):
-    counter = 0
-    total_time = 0.0
-    total_mem = 0.0
-
+    
     def wrapped_function(*args, **kwargs):
-        nonlocal counter, total_time, total_mem
         
+        wrapped_function.counter += 1
         tracemalloc.start()
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
         current_memory, peak_memory = tracemalloc.get_traced_memory()
         tracemalloc.stop()
-        
-        counter += 1
-        total_time += end_time - start_time
-        total_mem += peak_memory
 
-        wrapped_function.counter = counter
-        wrapped_function.total_time = total_time
-        wrapped_function.total_mem = total_mem
+        wrapped_function.total_time += (end_time - start_time)
+        wrapped_function.total_mem += peak_memory
 
         return result
 
-    wrapped_function.counter = counter
-    wrapped_function.total_time = total_time
-    wrapped_function.total_mem = total_mem
+    wrapped_function.counter = 0
+    wrapped_function.total_time = 0.0
+    wrapped_function.total_mem = 0.0
 
     return wrapped_function
