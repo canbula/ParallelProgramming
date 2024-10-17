11f818c334e2c63d508cc01d37f7877510f4b86c
Extocraftor <51536864+Extocraftor@users.noreply.github.com>
2024-10-17 20:33:40 +0300

    Create functions_mert_durgun.py

---
+++ Week04/functions_mert_durgun.py
@@ -0,0 +1,10 @@
+custom_power = lambda x=0, e=1: x ** e
+
+def custom_equation(x: int = 0, y: int = 0, a: int = 1, b: int = 1, *, c: int = 1) -> float:
+    return (a * x + b * y) / c
+
+counter = 0
+def fn_w_counter() -> (int, dict[str, int]):
+    global counter
+    counter += 1
+    return counter, {"functions_1": counter}
