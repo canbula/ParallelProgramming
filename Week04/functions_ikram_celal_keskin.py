692de0b06f652e1f38f54291bbc8c9a61849d323
icelal-kskn <72507616+icelal-kskn@users.noreply.github.com>
2024-10-19 16:18:14 +0300

    Update functions_ikram_celal_keskin.py
    Deleted global variables

--- Week04/functions_ikram_celal_keskin.py
+++ Week04/functions_ikram_celal_keskin.py
@@ -1,11 +1,5 @@
 
 
-
-count= 0 
-caller_dict= {}
-
-
-
 custom_power= lambda x=0,/,e=1: x**e
 
 def custom_equation(x:int=0,y:int=0,/,a:int=1,b:int=1,*,c:int=1)->float:
@@ -29,6 +23,8 @@ def custom_equation(x:int=0,y:int=0,/,a:int=1,b:int=1,*,c:int=1)->float:
     """
     return (x**a + y**b) / c
 
+
+
 def fn_w_counter()->(int, dict[str, int]):
     """
     This funtion counts the how many times it is called and from which caller it is called.
@@ -36,19 +32,20 @@ def fn_w_counter()->(int, dict[str, int]):
     :return: Returning integer is the total number of calls and Returning dictionary with string keys and integer values includes the caller ( _ _name_ _ ) as key, the number of call coming from this caller as value.
     :rtype: tuple(int, dict[str, int])
     """
-    global count
-    global caller_dict
+    if not hasattr(fn_w_counter, "count"):
+        setattr(fn_w_counter,"count",0)
+        setattr(fn_w_counter,"caller_dict",{})
+        
 
-    count += 1
+    fn_w_counter.count += 1
     caller= __name__
 
-    if caller not in caller_dict:
-        caller_dict[caller] = 0
+    if caller not in fn_w_counter.caller_dict:
+        fn_w_counter.caller_dict[caller] = 0
     
-    caller_dict[caller] += 1 
+    fn_w_counter.caller_dict[caller] += 1 
 
-    return (int(count),dict(caller_dict))
-
+    return (int(fn_w_counter.count),dict(fn_w_counter.caller_dict))
 
 
 
