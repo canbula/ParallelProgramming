10514caef18204f75d57f7adf58433d71e1075cb
Muhendish <105726903+Muhendish@users.noreply.github.com>
2024-10-21 18:28:41 +0300

    Update pyramid_hikmet_keserci.py

--- Week03/pyramid_hikmet_keserci.py
+++ Week03/pyramid_hikmet_keserci.py
@@ -1,22 +1,13 @@
 def calculate_pyramid_height(number_of_blocks):
-  height = 0
-  block_used = 0
-  layer = 1
+    height = 0
+    block_used = 0
+    layer = 1
 
-  while block_used + layer <= number_of_blocks:
-    blocks_used += layer
-    height += 1
-    layer += 1
+    while block_used + layer <= number_of_blocks:
+        block_used += layer
+        height += 1
+        layer += 1
 
-  missing_blocks = layer - (number_of_blocks - blocks_used) if blocks_used != number_of_blocks else 0
+    missing_blocks = layer - (number_of_blocks - block_used) if block_used != number_of_blocks else 0
 
-  return height, missing_blocks
-
-number_of_blocks = int(input("Enter the number of blocks for the pyramid: "))
-height, missing_blocks = calculate_pyramid_height(number_of_blocks)
-print(f"The height of the pyramid: {height}")
-
-if missing_blocks > 0:
-  print(f"Missing blocks to complete the pyramid: {missing_blocks}")
-else:
-  print("Pyramid is complete")
+    return height, missing_blocks
