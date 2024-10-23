eda1213204a318a0fea302c64ad48789766459ff
Hamza Kızıltoprak <127198357+HamzaKiziltoprak@users.noreply.github.com>
2024-10-23 11:24:17 +0300

    Update functions_hamza_kiziltoprak.py

--- Week04/functions_hamza_kiziltoprak.py
+++ Week04/functions_hamza_kiziltoprak.py
@@ -15,7 +15,7 @@ def custom_equation(x:int = 0,y:int=0,/,a:int = 1,b:int=1,*,c:int=1) -> float:
     return (x**a + y**b)/c
 
 def fn_w_counter() -> (int, dict[str, int]):
-    if not hasattr(fn_w_counter,'counter'):
+    if not hasattr(fn_w_counter,'_call_counter'):
         fn_w_counter._call_counter = 0
         fn_w_counter._caller_dict = {}
     caller = __name__
