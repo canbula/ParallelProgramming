3667cc5feb03174659958aea18acd9c7f138a612
Miray CENGİL <m.cengil48@gmail.com>
2024-10-23 20:55:40 +0300

    Create awaitme_miray_cengil.py

--- Week05/awaitme_miray_cengil.py
+++ Week05/awaitme_miray_cengil.py
@@ -9,7 +9,7 @@ def awaitme(func):
     """
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
-        if asyncio.iscoroutinefunction(func) 
+        if asyncio.iscoroutinefunction(func):
             return await func(*args, **kwargs)
         else:
             loop = asyncio.get_event_loop()
