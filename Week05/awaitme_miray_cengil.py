81a35b251aeb9865e5b390b583f22f5256e92e06
Miray CENGİL <m.cengil48@gmail.com>
2024-10-23 20:52:37 +0300

    Create awaitme_miray_cengil.py

---
+++ Week05/awaitme_miray_cengil.py
@@ -0,0 +1,18 @@
+import asyncio
+import functools
+
+def awaitme(func):
+    """
+    A decorator that turns any function into a coroutine.
+    It properly passes all arguments to the function.
+    If the function returns a value, the decorator will return it.
+    """
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        if asyncio.iscoroutinefunction(func) 
+            return await func(*args, **kwargs)
+        else:
+            loop = asyncio.get_event_loop()
+            return await loop.run_in_executor(None, functools.partial(func, *args, **kwargs))
+    
+    return wrapper
