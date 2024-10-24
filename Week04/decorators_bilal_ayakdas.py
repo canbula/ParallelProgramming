c3b964c897b28f2ccf822d8de3512b54fd56a361
Bilal Ayakdaş <72439364+Bilal-AYAKDAS@users.noreply.github.com>
2024-10-24 21:20:28 +0300

    Create decorators_bilal_ayakdas.py

---
+++ Week04/decorators_bilal_ayakdas.py
@@ -0,0 +1,27 @@
+from  time import time
+from  sys import getsizeof
+
+def performance(fn):
+
+    if not hasattr(performance,"counter"):
+        setattr(performance,"counter",0)
+    
+    if not hasattr(performance,"total_time"):
+        setattr(performance,"total_time",0.0)
+
+    if not hasattr(performance,"total_mem"):
+        setattr(performance,"total_mem",0)
+
+    
+    def calculate_perf(*args, **kwargs):
+        begin_time = time()
+        memory_usage = getsizeof(fn(*args,**kwargs))
+        end_time = time()
+
+        setattr(performance,"counter",getattr(performance,"counter") + 1)
+        setattr(performance,"total_time",getattr(performance,"total_time") + (end_time - begin_time))
+        setattr(performance,"total_time",getattr(performance,"total_mem") + memory_usage)
+
+        return fn(*args,**kwargs)
+    return calculate_perf
+
