b8eaa24ae3b429f32668507089a5813775118c1e
Berkin Yıldırım <161759242+berkinyl@users.noreply.github.com>
2024-10-26 21:18:41 +0300

    Create pyramid_berkin_yildirim.py

---
+++ Week03/pyramid_berkin_yildirim.py
@@ -0,0 +1,10 @@
+def calculate_pyramid_height(blocks):
+    height = 0
+
+    for level in range(1, blocks + 1):
+        if blocks >= level:
+            blocks -= level
+            height += 1
+        else:
+            break
+  return height
