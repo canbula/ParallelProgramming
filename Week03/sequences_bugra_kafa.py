38794179593985d88bc6649ad42f9483f69c2d4c
Buğra Kafa <135351301+BGR5@users.noreply.github.com>
2024-10-27 08:12:03 +0300

    Create sequences_bugra_kafa.py

---
+++ Week03/sequences_bugra_kafa.py
@@ -0,0 +1,25 @@
+def remove_duplicates(seq:list)->list:
+  """This function removes duplicates from a list"""
+  new_list = []
+  for i in seq:
+    if not i in new_list:
+      new_list.append(i) 
+  
+   return new_list
+     
+ 
+
+def list_counts(seq:list)->dict:
+  my_dict={}
+  
+  for i in seq:
+    my_dict[i] = seq.count(i)
+    
+  return my_dict
+
+def reverse_dict(d:dict) -> dict:
+  new_dict={}
+  for x,y in dict.items():
+    new_dict.update({y:x})
+  
+  return new_dict
