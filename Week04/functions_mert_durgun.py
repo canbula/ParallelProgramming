01e676172b3ed08505c8daff535f656a85ce4326
Extocraftor <51536864+Extocraftor@users.noreply.github.com>
2024-11-01 13:52:55 +0300

    Update functions_mert_durgun.py

--- Week04/functions_mert_durgun.py
+++ Week04/functions_mert_durgun.py
@@ -1,9 +1,16 @@
-custom_power = lambda x=0, e=1: x ** e
+custom_power = lambda x=0, /, e=1 :  x**e
 
-def custom_equation(x: int = 0, y: int = 0, a: int = 1, b: int = 1, *, c: int = 1) -> float:
-    return (a * x + b * y) / c
+def custom_equation(x: int = 0, y: int = 0,/, a: int = 1, b: int = 1, *, c: int=1) -> float:
+    return (x**a + y**b) / c
 
-counter = 0
-def fn_w_counter() -> (int, dict[str, int]):
-    counter += 1
-    return counter, {"functions_1": counter}
+
+def fn_w_counter() -> (int, dict[str,int]):
+
+    if not hasattr(fn_w_counter, "counter"):
+        fn_w_counter.counter = 0
+    
+    fn_w_counter.counter += 1
+    module_name = __name__
+    call_counter_dict = {module_name: fn_w_counter.counter}
+    
+    return fn_w_counter.counter, call_counter_dict
