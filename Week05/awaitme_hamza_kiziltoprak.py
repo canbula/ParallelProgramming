9a9a2dc6c8c2deb4d2d3eab7fdce1e37de1a7841
Hamza Kızıltoprak <127198357+HamzaKiziltoprak@users.noreply.github.com>
2024-11-03 00:37:20 +0300

    Create awaitme_hamza_kiziltoprak.py

---
+++ Week05/awaitme_hamza_kiziltoprak.py
@@ -0,0 +1,9 @@
+import asyncio
+
+def awaitme(func):
+    async def _wrapper(*args,**kwargs):
+        result = func(*args,**kwargs)
+        if asyncio.iscoroutine(result):
+            return await result
+        return result
+    return _wrapper
