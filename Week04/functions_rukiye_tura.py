462796884b5cce8019a84d281982a9ec6ba0395e
RukiyeTura <124088168+RukiyeTura@users.noreply.github.com>
2024-11-02 16:36:36 +0300

    Update functions_rukiye_tura.py

--- Week04/functions_rukiye_tura.py
+++ Week04/functions_rukiye_tura.py
@@ -1,23 +1,19 @@
-custom_power = lambda x=0, /, e=1: x ** e
+custom_power = lambda x=0,/,e=1 : x**e
 
-def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int = 1) -> float:
 
-    if c == 0:
-        raise ValueError("Division by Zero Exception")
+def custom_equation(x: int = 0, y: int = 0,/, a: int = 1, b: int = 1,*, c: int = 1) -> float:
 
-    return (x ** a + y ** b) / c
+    return (x**a + y**b) / c
+
 
 def fn_w_counter() -> (int, dict[str, int]):
-    
-    if not hasattr(fn_w_counter, 'call_count'):
-        fn_w_counter.call_count = 0
-        fn_w_counter.callers = {}
-    fn_w_counter.call_count += 1
-
-    caller_name = __name__
-
-    if caller_name in fn_w_counter.callers:
-        fn_w_counter.callers[caller_name] += 1
+    if not hasattr(fn_w_counter, "counter"):
+        fn_w_counter.counter = (0, {})
+    counter_num = fn_w_counter.counter[0]
+    counter_dict = fn_w_counter.counter[1]
+    if __name__ in counter_dict:
+        counter_dict[__name__] += 1
     else:
-        fn_w_counter.callers[caller_name] = 1
-    return fn_w_counter.call_count, fn_w_counter.callers
+        counter_dict[__name__] = 1
+    fn_w_counter.counter = (counter_num + 1, counter_dict)
+    return fn_w_counter.counter
