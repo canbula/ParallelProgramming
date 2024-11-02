cb8ad8d1f5c9ed10e6180c186c4e3c43f5892919
dilek <128893136+dilekk1@users.noreply.github.com>
2024-11-02 13:23:15 +0300

    Delete pyramid_dilek_celebi.py

--- pyramid_dilek_celebi.py
+++
@@ -1,7 +0,0 @@
-def calculate_pyramid_height(number_of_blocks):
-    used_before = 0
-    height = 0
-    while(number_of_blocks - used_before > height):
-       height = height + 1
-       used_before = (height*(height + 1)//2)
-    return height
