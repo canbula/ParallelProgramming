7875ef443dc7b68a6944d3949aa991ea053768cc
Esra Kaya <112573124+esrakaya64@users.noreply.github.com>
2024-11-20 09:25:46 +0300

    Update pyramid_esra_kaya.py

--- Week03/pyramid_esra_kaya.py
+++ Week03/pyramid_esra_kaya.py
@@ -1,9 +1,7 @@
-def calculate_pyramid_height(cube_number):
-    total_cube = 0
+def calculate_pyramid_height(number_of_blocks):
     height = 0
-    
-    while total_cube < cube_number:
+    total_blocks = 0
+    while total_blocks + height + 1 <= number_of_blocks:
         height += 1
-        total_cube += height
-        if total_cube == cube_number:
-            return height
+        total_blocks += height
+    return height
