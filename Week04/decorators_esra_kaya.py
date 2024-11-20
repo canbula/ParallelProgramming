97d69d6e5a8f2de526f64fb2484ef3da41a75f89
Esra Kaya <112573124+esrakaya64@users.noreply.github.com>
2024-11-20 09:29:34 +0300

    Update decorators_esra_kaya.py

--- Week04/decorators_esra_kaya.py
+++ Week04/decorators_esra_kaya.py
@@ -1,31 +1,26 @@
 import time
-import tracemalloc
+import sys
 
 def performance(func):
-    
-    def wrapper(*args, **kwargs):
-        if not hasattr(wrapper, "counter"):
-            wrapper.counter = 0
-            wrapper.total_time = 0
-            wrapper.total_mem = 0
 
-        tracemalloc.start()
-        start_time = time.time()
+    if not hasattr(performance, 'counter'):
+        setattr(performance, 'counter', 0)
+
+    if not hasattr(performance, 'total_time'):
+        setattr(performance, 'total_time', 0.0)
+    
+    if not hasattr(performance, 'total_mem'):
+        setattr(performance, 'total_mem', 0)
+
+    def wrapper(*args, **kwargs):
+        start_time = time.time()       
+        memory_usage=sys.getsizeof(func(*args, **kwargs))
+        end_time = time.time()             
+            
+        setattr(performance, 'counter', getattr(performance, 'counter') + 1)
+        setattr(performance, 'total_time', getattr(performance, 'total_time') + (end_time - start_time))
+        setattr(performance, 'total_mem', getattr(performance, 'total_mem') + memory_usage)
         
-        result = func(*args, **kwargs)
-        
-        time_taken = time.time() - start_time
-        current_mem, peak_mem = tracemalloc.get_traced_memory()
-        tracemalloc.stop()
-        
-        wrapper.counter += 1
-        wrapper.total_time += time_taken
-        wrapper.total_mem += peak_mem
-        
-        print(f"'{func.__name__}' was called {wrapper.counter} times.")
-        print(f"Total time so far: {wrapper.total_time:.4f} seconds.")
-        print(f"Total peak memory used: {wrapper.total_mem / 1024:.2f} KB.\n")
-        
-        return result
+        return func(*args, **kwargs)
 
     return wrapper
