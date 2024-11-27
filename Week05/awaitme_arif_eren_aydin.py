d008d74ff8bf6fd586a3af19046f27e3ed841608
ArifErenAydin-code <aydinariferen@gmail.com>
2024-11-27 13:23:20 +0300

    Delete Week05/awaitme_arif_eren_aydin.py

--- Week05/awaitme_arif_eren_aydin.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-def awaitme(func):
-    async def wrapper(*args, **kwargs):
-        if not asyncio.iscoroutinefunction(func):
-            return func(*args,**kwargs)
-        return await func(*args,**kwargs)
-    return wrapper
