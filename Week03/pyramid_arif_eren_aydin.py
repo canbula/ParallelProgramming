71c540b7cccec45dc860f63d3a781b9c1168bef0
ArifErenAydin-code <aydinariferen@gmail.com>
2024-11-27 13:22:26 +0300

    Delete Week03/pyramid_arif_eren_aydin.py

--- Week03/pyramid_arif_eren_aydin.py
+++
@@ -1,9 +0,0 @@
-def calculate_pyramid_height(number_of_blocks):
-    height = 0
-    for i in range(0,number_of_blocks):
-        number_of_blocks=number_of_blocks - (height+1) 
-        height += 1
-        i += 1
-        if height >= number_of_blocks :
-            break
-    return height
