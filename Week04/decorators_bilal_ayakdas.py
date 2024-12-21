af2efdc69aebc70c5ef9c44c64d20ba7863e7a5c
Bilal Ayakdaş <72439364+Bilal-AYAKDAS@users.noreply.github.com>
2024-12-21 21:23:46 +0300

    I used the tracemalloc module.

--- Week04/decorators_bilal_ayakdas.py
+++ Week04/decorators_bilal_ayakdas.py
@@ -1,27 +1,29 @@
-from  time import time
-from  sys import getsizeof
+from time import time
+import tracemalloc
+
 
 def performance(fn):
+    if not hasattr(performance, "counter"):
+        setattr(performance, "counter", 0)
 
-    if not hasattr(performance,"counter"):
-        setattr(performance,"counter",0)
-    
-    if not hasattr(performance,"total_time"):
-        setattr(performance,"total_time",0.0)
+    if not hasattr(performance, "total_time"):
+        setattr(performance, "total_time", 0.0)
 
-    if not hasattr(performance,"total_mem"):
-        setattr(performance,"total_mem",0)
+    if not hasattr(performance, "total_mem"):
+        setattr(performance, "total_mem", 0)
 
-    
     def calculate_perf(*args, **kwargs):
-        begin_time = time()
-        result = fn(*args,**kwargs)
-        memory_usage = getsizeof(result)
-        end_time = time()
 
-        setattr(performance,"counter",getattr(performance,"counter") + 1)
-        setattr(performance,"total_time",getattr(performance,"total_time") + (end_time - begin_time))
-        setattr(performance,"total_mem",getattr(performance,"total_mem") + memory_usage)
+        tracemalloc.start()
+        begin_time = time()
+        result = fn(*args, **kwargs)
+        end_time = time()
+        current, peak = tracemalloc.get_traced_memory()
+
+        setattr(performance, "counter", getattr(performance, "counter") + 1)
+        setattr(performance, "total_time", getattr(performance, "total_time") + (end_time - begin_time))
+        setattr(performance, "total_mem", getattr(performance, "total_mem") + peak)
 
         return result
+
     return calculate_perf
