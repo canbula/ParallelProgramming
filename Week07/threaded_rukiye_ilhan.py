980d1816d9ad0000ac3965265ae052f444825ea3
rukiye-ilhan <157627117+rukiye-ilhan@users.noreply.github.com>
2024-12-26 16:43:52 +0300

    Update threaded_rukiye_ilhan.py

--- Week07/threaded_rukiye_ilhan.py
+++ Week07/threaded_rukiye_ilhan.py
@@ -1,13 +1,11 @@
 import threading
-def threaded(n):
-    def decorator(func):
-        def wrapper(*args,**kwargs):
-            threads = []
-            for i in range(n):
-                t = threading.Thread(target = func,args=args, kwargs=kwargs)
-                threads.append(t)
-                t.start()
-            for thread in threads:
-                thread.join()
-        return wrapper
-    return decorator
+def threaded(func):
+    def wrapper(*args, **kwargs):
+        threads = []
+        for _ in range(args[0]):  # args paramtre listesindeki 0. eleman n değeridir dolaylı olarak elde edildi
+            t = threading.Thread(target=func, args=args[1:], kwargs=kwargs)#args=args[1:] 1.indeksetiki 'Merhaba Dinbya' yazısını basar
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
+    return wrapper
