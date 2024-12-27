cde3abcf56deb8b2ba80f770667a4df17db53d77
rukiye-ilhan <157627117+rukiye-ilhan@users.noreply.github.com>
2024-12-27 19:29:10 +0300

    Update threaded_rukiye_ilhan.py

--- Week07/threaded_rukiye_ilhan.py
+++ Week07/threaded_rukiye_ilhan.py
@@ -1,14 +1,17 @@
 import threading
 
-def threaded(func):
-    def wrapper(n, *args, **kwargs):
+def threaded(n):
+    """İlk fonksiyon - thread sayısını parametre olarak alır"""
+    def wrapper(func):
+        """İkinci fonksiyon - thread'leri oluşturur ve yönetir"""
         threads = []
+        #istenen n sayıdaki tjread oluşturldu
         for _ in range(n):
-            t = threading.Thread(target=func, args=args, kwargs=kwargs)
+            t = threading.Thread(target=func)
             threads.append(t)
             t.start()
-        for t in threads:
-            t.join()
+        # Bütün thread'lerin bitmesini bekle
+        for thread in threads:
+            thread.join()
+        return func  # Orijinal fonksiyonu döndür  
     return wrapper
-
-
