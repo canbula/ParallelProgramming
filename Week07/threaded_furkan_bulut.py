c16f57e30db68a8bc88f69cb689bfb5fb07b9687
Furkan Bulut <93736726+Furk4nBulut@users.noreply.github.com>
2024-12-28 13:00:30 +0300

    Class based threaded_furkan_bulut.py

--- Week07/threaded_furkan_bulut.py
+++ Week07/threaded_furkan_bulut.py
@@ -2,41 +2,52 @@ import threading
 import time
 
 def threaded(number_of_threads):
-    """
-    A decorator that runs a function concurrently using multiple threads.
-
-    :param number_of_threads: The number of threads to spawn to run the decorated function.
-    :type number_of_threads: int
-
-    :return: A decorator to wrap a function with threaded execution.
-    :rtype: function
-    """
-    def decorator(func):
+    class Threaded:
         """
-        The actual decorator that spawns the threads and runs the function.
+        A class that acts as a decorator to run a function concurrently using multiple threads.
 
-        :param func: The function to be executed in multiple threads.
-        :type func: function
-
-        :return: The wrapper function that creates threads and runs the decorated function.
-        :rtype: function
+        :param number_of_threads: The number of threads to spawn to run the decorated function.
+        :type number_of_threads: int
         """
-        def wrapper(*args, **kwargs):
+        def __init__(self, number_of_threads):
             """
-            Wrapper function that creates threads and runs the decorated function.
+            Initializes the Threaded class with the specified number of threads.
 
-            :param args: Positional arguments to pass to the decorated function.
-            :type args: tuple
-
-            :param kwargs: Keyword arguments to pass to the decorated function.
-            :type kwargs: dict
+            :param number_of_threads: Number of threads to use for the decorated function.
+            :type number_of_threads: int
             """
-            threads = []
-            for i in range(number_of_threads):
-                thread = threading.Thread(target=func, args=args, kwargs=kwargs, name=f"Thread-{i}")
-                threads.append(thread)
-                thread.start()
-            for thread in threads:
-                thread.join()
-        return wrapper
-    return decorator
+            self.number_of_threads = number_of_threads
+
+        def __call__(self, func):
+            """
+            Makes the class instance callable and acts as a decorator for the provided function.
+
+            :param func: The function to be executed in multiple threads.
+            :type func: function
+
+            :return: The wrapper function that creates threads and runs the decorated function.
+            :rtype: function
+            """
+            def wrapper(*args, **kwargs):
+                """
+                Wrapper function that creates threads and runs the decorated function.
+
+                :param args: Positional arguments to pass to the decorated function.
+                :type args: tuple
+
+                :param kwargs: Keyword arguments to pass to the decorated function.
+                :type kwargs: dict
+                """
+                threads = []
+
+                for i in range(self.number_of_threads):
+                    thread = threading.Thread(target=func, args=args, kwargs=kwargs, name=f"Thread-{i}")
+                    threads.append(thread)
+                    thread.start()
+
+                for thread in threads:
+                    thread.join()
+
+            return wrapper
+
+    return Threaded(number_of_threads)
