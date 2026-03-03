88942c1294d6e7b69cfbc958268c15c74e5a1d7d
Ebrukoksal <114864294+Ebrukoksal@users.noreply.github.com>
2026-03-03 12:27:33 +0300

    week03 homework

---
+++ Week03/sequences_ebru_koksal.py
@@ -0,0 +1,24 @@
+def remove_duplicates(seq: list) -> list:
+  """ This function removes duplicates from a list"""
+  result = []
+  for item in seq:
+    if item not in result:
+      result.append(item)
+  return result
+
+def list_counts(seq: list) -> dict:
+  """This function counts the number of occurrences of each item in a list"""
+  counts = {}
+  for item in seq:
+    if item in counts:
+      counts[item] += 1
+    else:
+      counts[item] = 1
+  return counts
+
+def reverse_dict(d: dict) -> dict:
+  """This function reverses the keys and values of a dictionary"""
+    reversed_dict = {}
+    for key, value in d.items():
+        reversed_dict[value] = key
+    return reversed_dict
