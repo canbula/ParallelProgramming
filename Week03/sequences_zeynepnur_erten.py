8fb7d6cf74015b0ab5bd328e9b9d25d6afb054bd
zeynep nur erten <zeynepnurerten4@gmail.com>
2026-03-03 12:30:24 +0300

    Refactor list_counts and reverse_dict functions

--- Week03/sequences_zeynepnur_erten.py
+++ Week03/sequences_zeynepnur_erten.py
@@ -8,16 +8,16 @@ def remove_duplicates( seq:list ) -> list:
   
 
 def list_counts(seq: list) -> dict:
-    counts = {}
-    for item in seq:
-        counts[item] = counts.get(item, 0) + 1
+  counts = {}
+  for item in seq:
+    counts[item] = counts.get(item, 0) + 1
   
   return counts
 
 
 
 def reverse_dict(d: dict) -> dict:
-    new_d = {}
-    for k, v in d.items():
-        new_d[v] = k 
+  new_d = {}
+  for k, v in d.items():
+    new_d[v] = k 
   return new_d
