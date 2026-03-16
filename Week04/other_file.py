a553d12658ceae5a52dc51f86bdc8885cef67aba
Bora Canbula <bora.canbula@cbu.edu.tr>
2026-03-17 00:24:50 +0300

    Tests 4 W04

---
+++ Week04/other_file.py
@@ -0,0 +1,12 @@
+class OtherClass:
+    puclic_var = 1
+    _protected_var = 2
+    __private_var = 3
+
+
+def public_function():
+    print("I am a public function")
+
+
+def _protected_function():
+    print("I am a protected function")
