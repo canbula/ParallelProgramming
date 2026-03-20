efd5bfe8771056e0a759d1fa99fa07e4fa203341
Elif Yalvaç <59252046+elifyalvac@users.noreply.github.com>
2026-03-20 21:44:06 +0300

    Update decorators_elif_yalvac.py

--- Week04/decorators_elif_yalvac.py
+++ Week04/decorators_elif_yalvac.py
@@ -1,28 +1,27 @@
 import time
 import tracemalloc
-from functools import wraps
 
 def performance(func):
-    @wraps(func)
     def wrapper(*args, **kwargs):
         # Sayaç artır
-        performance.counter += 1
-        # Zaman ölçümü başlat
-        start_time = time.perf_counter()
+        performance.counter += 1        
         # Bellek ölçümü başlat
         tracemalloc.start()
+        # Zaman ölçümü başlat
+        start_time = time.perf_counter()       
         # Fonksiyonu çalıştır
-        result = func(*args, **kwargs)
-        # Bellek ölçümü bitir
-        current, peak = tracemalloc.get_traced_memory()
-        tracemalloc.stop()
+        result = func(*args, **kwargs)        
         # Zaman ölçümü bitir
         end_time = time.perf_counter()
-        # Toplam süreyi ekle
+        # Bellek ölçümü bitir (peak ve current değerleri al)
+        current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()  
+        # Toplam süreyi ve belleği ekle
         performance.total_time += (end_time - start_time)
-        # Toplam bellek (peak değer)
         performance.total_mem += peak
         return result
+    wrapper.__name__ = func.__name__
+    wrapper.__doc__ = func.__doc__
     return wrapper
 
 performance.counter = 0
