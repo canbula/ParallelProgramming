d1165b940cf06de388d6f3257f03f21a9fc818f4
İsmail Esat Uluöz <161027888+Ismail-Esat-Uluoz@users.noreply.github.com>
2026-04-02 16:22:18 +0300

    Create pyramid_ismailesat_uluoz.py

---
+++ Week03/pyramid_ismailesat_uluoz.py
@@ -0,0 +1,11 @@
+def calculate_pyramid_height(number_of_blocks):
+    height = 0
+
+    while number_of_blocks >= 1:
+        if number_of_blocks >= height + 1:
+            height += 1
+            number_of_blocks -= height
+        else:
+            break
+
+    return height
