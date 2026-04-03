20db4bf203dc8ea58af3d6001bfa70a4e43f8797
zainebkchaou <163732105+zainebkchaou@users.noreply.github.com>
2026-04-03 19:46:49 +0300

    Create pyramid_height_zaineb_kchaou.py

---
+++ Week03/pyramid_height_zaineb_kchaou.py
@@ -0,0 +1,11 @@
+def calculate_pyramid_height(number_of_blocks):
+    height = 0
+    blocks_needed = 1
+    while number_of_blocks >= blocks_needed:
+        number_of_blocks -= blocks_needed
+        height += 1
+        blocks_needed += 1
+    return height
+
+c= calculate_pyramid_height(6)
+print(c)
