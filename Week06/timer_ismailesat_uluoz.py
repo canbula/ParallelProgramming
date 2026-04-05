543145e6dcbe2172937d9d7973b2d60077bcb2f1
İsmail Esat Uluöz <161027888+Ismail-Esat-Uluoz@users.noreply.github.com>
2026-04-05 21:48:52 +0300

    Create timer_ismailesat_uluoz.py

---
+++ Week06/timer_ismailesat_uluoz.py
@@ -0,0 +1,13 @@
+import time
+
+class Timer:
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.end_time = time.time()
