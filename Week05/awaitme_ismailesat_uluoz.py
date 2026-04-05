254471a8edc99f8835a239dcde710d0e1631f6d1
İsmail Esat Uluöz <161027888+Ismail-Esat-Uluoz@users.noreply.github.com>
2026-04-05 20:35:49 +0300

    Create awaitme_ismailesat_uluoz.py

---
+++ Week05/awaitme_ismailesat_uluoz.py
@@ -0,0 +1,13 @@
+import asyncio
+
+def awaitme(func):
+
+    async def _awaitme(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        if asyncio.iscoroutine(result):
+            return await result
+
+        return result
+
+    return _awaitme
