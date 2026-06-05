594e8b07c9c662d36b4a155723a7f576030e1621
Mehmet Balcı <148548201+MehmetxBalci@users.noreply.github.com>
2026-06-05 13:27:58 +0300

    Add threaded decorator for concurrent function execution

---
+++ Week07/threaded_mehmet_balci.py
@@ -0,0 +1,16 @@
+import threading
+
+def threaded(n):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            # List comprehension ile append overhead'inden kurtularak her seferinde YENİ thread yaratıyoruz
+            threads = [threading.Thread(target=func, args=args, kwargs=kwargs) for _ in range(n)]
+            
+            for t in threads:
+                t.start()
+                
+            for t in threads:
+                t.join()
+                
+        return wrapper
+    return decorator
